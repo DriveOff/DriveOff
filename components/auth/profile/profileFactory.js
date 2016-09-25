ed5bdfa29a973356b9590bb00b0ad070aefb047b
Gwendolyn Holley <gmfholley@gmail.com>
2016-09-25 15:27:12 -0500

    Second version of production app

--- components/auth/profile/profileFactory.js
+++ components/auth/profile/profileFactory.js
@@ -1,11 +1,24 @@
-function profileFactory(localStorageService) {
+function profileFactory($injector) {
+  
+  var localStorageService = $injector.get('localStorageService');
   
   var profile = {
     name: localStorageService.get("name"),
     email: localStorageService.get("email"),
     points: localStorageService.get("points"),
     avatar: localStorageService.get("avatar"),
-    id: localStorageService.get("id")
+    id: localStorageService.get("id"),
+    errors: []
+  }
+  
+  var putURL = "https://driveoff.herokuapp.com/users";
+  
+  //TODO - add correct XHR here for updating
+  
+  profile.updateProfile = function() {
+    
+    
+    
   }
 
   return profile;
